251cd04b4a98a4de050e50b3383a6683cec81473
Evan Dillon <evan.dillon@gmail.com>
2017-03-02 08:54:42 -0700

    adding options to args list, moving all functionality to service

--- src/twitter-tweet.component.ts
+++ src/twitter-tweet.component.ts
@@ -11,31 +11,20 @@ import { TwitterWidgetService } from './twitter-widget.service';
 export class TwitterTweetComponent implements OnInit, AfterViewInit
 {
 	@Input() tweetId: string;
+	@Input() options?:any;
 
 	constructor(private element: ElementRef, private twitterService : TwitterWidgetService){ }
 
 	ngOnInit(){ }
 
 	ngAfterViewInit() {
-		//MAKE SURE TWITTER WIDGET SCRIPT IS LOADED IN HEAD...
-		this.twitterService.LoadScript().subscribe (twttr => {
-			//SUCCESS, WE HAVE TWITTER WIDGETS JS FILE LOADED...
-			let nativeElement = this.element.nativeElement;
-
-			(<any>window)['twttr'].widgets.createTweet(this.tweetId, nativeElement, {}).then(function success(embed:any) {
-					//console.log('Created tweet widget: ', embed);
-			}).catch(function creationError(message:any) {
-					//console.log('Could not create widget: ', message);
-			});				
-		}, err => { 
-			//ERROR
-			console.log('****  ERROR LOADING TWITTER WIDGET', err);
-		}, () => {
-			//COMPLETE
-		});
+		this.options = this.options || {};
+		this.twitterService.createTweet(this.tweetId, this.element, this.options).then((response:any)=>{
+			//success
+			console.log('createTweet::Tweet Created');
+		}).catch((error:any)=>{
+			//failure
+			console.log('createTweet::Error');
+		})
 	}
-
-	private onTwitterLoaded(twttr:any){
-		console.log('TWITTER LOADED YO', twttr);
-	};
 }
